3299f7c4beb05d973b3204413f2ba6f68b2fd6f2
Thomas Quinlan <tquinlan1992@gmail.com>
2018-05-28 18:06:22 -0400

    add tests for getCreators

--- tquinlan1992-typescript-react-redux-utils/src/index.test.ts
+++ tquinlan1992-typescript-react-redux-utils/src/index.test.ts
@@ -1,5 +1,22 @@
+import { getCreators } from 'tquinlan1992-typescript-react-redux-utils';
+
 describe('1 test', () => {
     it('should pass', () => {
         expect(true).toBe(true);
+    });
+});
+
+describe('when getCreators is given', () => {
+    describe('an empty object', () => {
+        it('getCreators should return an empty object', () => {
+            const creators = getCreators({});
+            expect(creators).toMatchObject({});
+        });
+    });
+    describe('with an object without a actionCreator', () => {
+        it('getCreators should return an empty object', () => {
+            const creators = getCreators({noActionCreator: {} as any});
+            expect(creators).toMatchObject({});
+        });
     });
 });
