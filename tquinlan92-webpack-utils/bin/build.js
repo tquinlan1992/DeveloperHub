4409deb7496dae64b09e358bc58cf54854950685
Thomas Quinlan <thomasquinlan@Thomass-MBP.fios-router.home>
2018-07-28 13:37:34 -0400

    install npm

--- tquinlan92-webpack-utils/bin/build.js
+++ tquinlan92-webpack-utils/bin/build.js
@@ -5,6 +5,7 @@ const compiler = require('./compiler');
 compiler.run((err, stats) => {
     if (err || stats.hasErrors()) {
       // Handle errors here
+      console.log('stats', stats);
     }
     // Done processing
 });
