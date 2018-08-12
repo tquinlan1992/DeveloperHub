28d969df30a4ba0c9909096e2f59b337f95c4d6e
Thomas Quinlan <thomasquinlan@Thomass-MBP.fios-router.home>
2018-08-12 16:33:18 -0400

    set up all-projects

--- generator-tquinlan92/app/index.js
+++ generator-tquinlan92/app/index.js
@@ -1,12 +1,17 @@
 'use strict';
 const Generator = require('yeoman-generator');
+const path = require('path');
 
 const projectTypes = {
   library: "Library",
-  uiProject: "Ui Project",
-  storybook: "Storybook"
+  uiProject: "UI Project",
+  storybook: "Storybook",
+  uIComponentsStorybook: "UI with Components Library and Storybook"
 }
 
+function getDestionationPath(destinationPath) {
+  return destinationPath ? path.resolve(destinationPath, "./") : "./";
+}
 
 module.exports = class extends Generator {
 
@@ -17,9 +22,10 @@ module.exports = class extends Generator {
       name: 'buildType',
       message: 'What do you want to make today?',
       choices: [ 
-        projectTypes.library, 
-        projectTypes.uiProject, 
-        projectTypes.storybook 
+        projectTypes.library,
+        projectTypes.uiProject,
+        projectTypes.storybook,
+        projectTypes.uIComponentsStorybook
       ]
       }
     ]);
@@ -55,44 +61,86 @@ module.exports = class extends Generator {
       case (projectTypes.storybook):
         this._writeStorybook();
         break;
+      case (projectTypes.uIComponentsStorybook): 
+        this._writeUiComponentsStorybook();
+        break;
     }
   }
 
-  _writeLibrary() {
+  _writeUiComponentsStorybook() {
+    this._writeLibrary('./components');
+    this._writeUiProject('../app');
+    this._writeStorybook('../storybook');
+    this.fs.copy(
+      [
+      this.templatePath('all-projects/**/*'),
+      this.templatePath('all-projects/.*')
+      ],
+      this.destinationRoot('../')
+    ); 
+  }
+
+  _writeLibrary(destinationPath) {
+    destinationPath = getDestionationPath(destinationPath);
     this.fs.copy(
       [
       this.templatePath('library/**/*'),
-      this.templatePath('library/.*')
+      this.templatePath('library/.*'),
+      this.templatePath('library/.vscode/**/*')
       ],
-      this.destinationRoot('./')
+      this.destinationRoot(destinationPath)
     ); 
   }
   
-  _writeUiProject() {
+  _writeUiProject(destinationPath) {
+    destinationPath = getDestionationPath(destinationPath);
     this.fs.copy(
       [
       this.templatePath('ui/**/*'),
-      this.templatePath('ui/.*')
+      this.templatePath('ui/.*'),
+      this.templatePath('ui/.vscode/**/*')
       ],
-      this.destinationRoot('./')
+      this.destinationRoot(destinationPath)
     );
   }
   
-  _writeStorybook() {
+  _writeStorybook(destinationPath) {
+    destinationPath = getDestionationPath(destinationPath);
     this.fs.copy([
       this.templatePath('storybook/**/*'),
       this.templatePath('storybook/.*'),
-      this.templatePath('storybook/.storybook/**/*')
+      this.templatePath('storybook/.storybook/**/*'),
+      this.templatePath('storybook/.vscode/**/*')
     ],
-      this.destinationRoot('./')
+      this.destinationRoot(destinationPath)
     );
   }
 
-  install() {
-    this.npmInstall();
+  _createInstallSpawnPromise(destinationPath) {
+    return new Promise((resolve, reject) => {
+      const componentsSpawn = this.spawnCommand("npm", ["install"], { cwd: this.destinationPath(destinationPath)});
+      componentsSpawn.on('close', (code) => {
+        resolve();
+      });
+    });
   }
 
-  end() {
+  async install() {
+    if(this.projectType === projectTypes.uIComponentsStorybook) {
+      await Promise.all([
+        this._createInstallSpawnPromise('./components'),
+        this._createInstallSpawnPromise('./app'),
+        this._createInstallSpawnPromise('./storybook')
+      ]);
+      await this._createInstallSpawnPromise('./');
+      this._end();
+    } else {
+      await this._createInstallSpawnPromise('./');
+      this._end();
+    }
+  }
+
+  _end() {
     this.log(`You're done!`);
   }
 
