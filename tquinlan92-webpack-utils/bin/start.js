ac7edb93d7fde0f5c95a13d94ba2214a96c8ece2
Thomas Quinlan <thomasquinlan@Thomass-MBP.fios-router.home>
2018-08-21 02:08:18 -0400

    use gulp

--- tquinlan92-webpack-utils/bin/start.js
+++ tquinlan92-webpack-utils/bin/start.js
@@ -1,11 +1,35 @@
-#!/usr/bin/env node
+const gulp = require('gulp');
+const typescript = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
 
-const compiler = require('./compiler');
-const webpackConfig = require('../src/webpack.config');
-const compilerHandler = require('./compilerHandler');
+process.chdir(process.env.PWD);
 
-compiler(webpackConfig).watch({
-    // Example watchOptions
-    aggregateTimeout: 300,
-    poll: undefined
-  }, compilerHandler);
+gulp.task('assets' , () => {
+    gulp.src('./assets/**/*').pipe((gulp.dest('./dist/assets')));
+});
+
+const typescriptProject = typescript.createProject('./tsconfig.json', {
+  inlineSourceMap: false,
+  inlineSources: false 
+});
+
+gulp.task('tsc', () => {
+    const compiled = typescriptProject.src().pipe(sourcemaps.init()).pipe(typescriptProject());
+    compiled.pipe(sourcemaps.write());
+    compiled.js.pipe(gulp.dest('./dist'));
+    compiled.dts.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('ci', ['tsc', 'assets']);
+
+gulp.task('watchTs', () => {
+    gulp.watch(['./src/**/*'], ['tsc']);
+});
+
+gulp.task('watchAssets', () => {
+    gulp.watch(['./assets/**/*'], ['assets']);
+});
+
+gulp.task('watch', ['ci', 'watchTs', 'watchAssets']);
+
+gulp.task('default', ['watch']);
