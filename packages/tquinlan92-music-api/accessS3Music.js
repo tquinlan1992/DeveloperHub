3adc1d3c3df88acaa7bdb5d0cbfe2c232d979e8f
tquinlan1992 <tquinlan1992@gmail.com>
2018-11-17 13:49:08 -0500

    accessS3Music

--- packages/tquinlan92-music-api/accessS3Music.js
+++ packages/tquinlan92-music-api/accessS3Music.js
@@ -40,3 +40,28 @@ exports.handler = (event, context, callback) => {
     }
   });
 };
+
+
+
+
+
+var AWS = require('aws-sdk'),
+  uuid = require('uuid'),
+  documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = (event, context, callback) => {
+  var searchParams = {
+    Key: {
+      "songID": "7eceba60-e946-11e8-85b6-1df10a2f3069"
+    },
+    TableName: 'tquinlan92Music'
+  };
+  documentClient.get(searchParams, function(err, data) {
+    if (data) {
+      callback(null, data.Item.playCount);
+    } else {
+        console.log('err', err);
+        callback(null, err)
+    }
+  });
+};
